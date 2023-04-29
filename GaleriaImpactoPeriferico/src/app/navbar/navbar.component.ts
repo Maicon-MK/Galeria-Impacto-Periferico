1a960a0e4bb40af0050597f7c2aa11fe8e46818c
MateusMo <mat.marq.oliveira@gmail.com>
2023-04-29 11:04:12 -0300

    Feature: Metamask Integration

--- GaleriaImpactoPeriferico/src/app/navbar/navbar.component.ts
+++ GaleriaImpactoPeriferico/src/app/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import detectEthereumProvider from '@metamask/detect-provider';
+import { WalletService } from '../services/wallet.service';
+
+
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  constructor(private wallet:WalletService) { }
 
   ngOnInit(): void {
   }
 
+  async connect() {
+    const provider = await detectEthereumProvider() as any;
+  
+    if (provider) {
+      // Prompt the user to connect to Metamask
+      this.wallet.address = await provider.request({ method: 'eth_requestAccounts' });
+      console.log(this.wallet.address)
+    } else {
+      console.error('Metamask not found');
+    }
+  }
+  
 }
+
+
